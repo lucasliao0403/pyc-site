0bc85489c486557d28cf5c76871dcbceced58fdc
lucasliao0403 <60577714+lucasliao0403@users.noreply.github.com>
2023-10-22 18:23:03 -0400

    added footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Image from 'next/image'
+import styles from '/styles/footer.module.css'
+
 
 function Footer(props) {
     return (
-        <div>
-            footer
+        <div className={styles.footer}>
+            <div className={styles.grid}>
+                <div className={styles.bg}>
+                    <Image
+                    draggable={false}
+                    src={require("/assets/footerbg.png")}
+                    fill={true}
+                    style={{objectFit: "cover"}}
+                    alt="footer background"
+                    />
+                </div>
+                <div className={styles.content}>
+                    content
+                </div>
+            </div>
         </div>
     );
 }
